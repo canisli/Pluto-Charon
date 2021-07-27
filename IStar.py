82e87dfb4c23ea4de0f57af48d750e9a5c5762eb
Canis Li <73449574+canisli@users.noreply.github.com>
2021-07-26 22:31:54 -0700

    finish params and add csv support for IStar

--- IStar.py
+++ IStar.py
@@ -3,12 +3,19 @@ class IStar:
     Each instance of IStar describes a single star
     """
 
-    def __init__(self, star_name, x, y, magnitude, counts):
-        self.star_name = star_name
-        self.x = x
-        self.y = y
-        self.magnitude = magnitude
-        self.counts = counts
+    def __init__(self, star_name=None, x=None, y=None, magnitude=None, counts=None, table_row=None):
+        if table_row is None:
+            self.star_name = star_name
+            self.x = x
+            self.y = y
+            self.magnitude = magnitude
+            self.counts = counts
+        else:
+            self.star_name = table_row['name']
+            self.x = float(table_row['x'])
+            self.y = float(table_row['y'])
+            self.magnitude = float(table_row['mag'])
+            self.counts = float(table_row['counts'])
 
     def to_list(self):
         return [self.star_name, self.x, self.y, self.magnitude, self.counts]
