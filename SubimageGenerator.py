ebcc58322615ecd18a8b44b805b512f03d4ef44a
Canis Li <73449574+canisli@users.noreply.github.com>
2021-12-18 16:12:43 -0800

    finished

---
+++ SubimageGenerator.py
@@ -0,0 +1,46 @@
+# Generate subimages from an image file
+# Useful for debugging GaussianModel
+# Assumes that a starlist.csv file has already been created in the same directory
+
+import glob
+import tempfile
+import subprocess
+from pathlib import Path
+
+from astropy.table import Table
+
+from Image import Image
+from IStar import IStar
+
+SL = 19 # side length of subimage
+home = str(Path.home())
+path = home + "/dev/Pluto-Charon/data/"
+
+outside = True
+inside = True
+
+while outside:
+    i = 0
+    fits = sorted([x for x in glob.glob(path + "**/*.fits", recursive=True) if "1x1" not in x])
+    print("\nFits files:")
+    for f in fits:
+        print(f + " [" + str(i) + "]")
+        i += 1
+    fn = int(input("\nChoose file: "))
+    
+    image = Image(fits[fn])
+    
+    while inside:
+        starlist = Table.read(fits[fn][0:fits[fn].rfind('/')]+"/starlist.csv", format="csv")
+        
+        print("\nFirst 10 stars:")
+        for i in range(10):
+            star = IStar(table_row=starlist[i])
+            entry = star.star_name + " (" + str(star.x) + ", " + str(star.y) +")"
+            print(entry + ((58-len(entry)) * " ") + "[" + str(i) + "]")
+        sn = int(input("\nChoose star: "))    
+        
+        star = IStar(table_row=starlist[sn])
+        temp = tempfile.NamedTemporaryFile(prefix=star.star_name, suffix='.fits')
+        image.subimage(star.x+1, star.y+1, 19, 19).write_fits(temp.name)
+        subprocess.call(['open', temp.name])
