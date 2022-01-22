1f54bc948b538476d92eb093a304727d57e78383
Canis Li <73449574+canisli@users.noreply.github.com>
2022-01-21 18:31:18 -0800

    star gaussian toggle

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ min_distance = 50  # minimum distance between stars to perform Gaussian
 
 do_debugging_for_gaussian = True  # print Gaussian stats
 do_pauses_for_gaussian = True  # wait for user to press enter in between each Gaussian
+
+do_star_gaussian = True # toggle between star and pluto-charon gaussian
