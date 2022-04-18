fcef8587ccf5d033032a0d0570aa9475368f6cfb
Canis Li <73449574+canisli@users.noreply.github.com>
2022-04-17 21:20:17 -0700

    investigate more stars

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
 constants = {
     # GSC06325-02056   located at x=713.88 and y=552.25 with magnitude 11.11 and counts 1521000.0
     '4-25-2021' : {
+        # Pluto Charon
         'sigma_x2' : 3.142065240951312,
         'sigma_y2' : 3.6313832472241434,
         'blob_center_x' : 636.87 + 2,
@@ -9,12 +10,20 @@ constants = {
         'background': 4000,
         'init_Ap_coeff': 0.844,
         'init_Ac_coeff': 0.156,
+        # Nearby test star
         'nearby_star_counts' : 1521000,
         'nearby_star_x' : 713.88 + 2 ,
-        'nearby_star_y' : 552.25 + 1
+        'nearby_star_y' : 552.25 + 1,
+        'nearby_star2_counts' : 1621000.0,
+        'nearby_star2_x' : 620.14 + 1,
+        'nearby_star2_y' : 877.52 +1,
+        'nearby_star3_counts' :  917800.0,
+        'nearby_star3_x' : 758.8 + 1,
+        'nearby_star3_y' : 842.05 +1
     },
    # N/A located at x=652.1 and y=575.9 with magnitude N/A and counts 8423.0
     '5-17-2021' : {
+        #Pluto Charon
         'sigma_x2' : 2.2800575182901155,
         'sigma_y2' : 2.750866437919281,
         'blob_center_x' : 617.04 + 1,
@@ -23,8 +32,15 @@ constants = {
         'background': 4000,
         'init_Ap_coeff' : 0.844,
         'init_Ac_coeff' : 0.156,
+        # Nearby test star
         'nearby_star_counts' : 8423,
         'nearby_star_x' : 652.1 + 1,
-        'nearby_star_y' : 575.9 + 2
+        'nearby_star_y' : 575.9 + 2,
+        'nearby_star2_counts' : 1285000.0,
+        'nearby_star2_x' : 984.29 + 1,
+        'nearby_star2_y' : 725.02 +1,
+        'nearby_star3_counts' : 756000.0,
+        'nearby_star3_x' : 1029.3  + 1,
+        'nearby_star3_y' : 1014.1  +1
     }
 }
